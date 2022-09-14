a00c0a5af6e46e4c986c4111b7712d90df82f66c
hendrasetyawan <hendrasetyawan1945@gmail.com>
2022-09-14 14:14:35 +0700

    nama pengembalian buku

--- myaddos/perpus/models/pengembalian.py
+++ myaddos/perpus/models/pengembalian.py
@@ -8,8 +8,16 @@ class pengembalian(models.Model):
     name = fields.Char(
         compute='_compute_nama_peminjam',
         string='nama peminjam')
+    peminjaman_id = fields.Many2one(
+        comodel_name='p.peminjaman',
+        string='Peminjaman_id',
+        required=False)
 
-    @api.depends('name')
+    @api.depends('peminjaman_id')
     def _compute_nama_peminjam(self):
         for record in self:
-            record.name = self.env['p.peminjaman'].search([('id', '=', record.kd_register.id)]).mapped('no_anggota')
+            record.name = self.env['p.peminjaman'].search([('id', '=', record.peminjaman_id.id)]).mapped('nama_peminjaman')
+
+    tgl_pengembalian = fields.Date(
+        string='Tanggal Kembali',
+        default=fields.Date.today())
